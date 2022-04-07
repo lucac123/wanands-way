475bed193af3a446158297444ed4c15c95473673
Luca <lchackalackal@gmail.com>
2022-04-07 01:08:45 +0000

    did something ig

---
+++ src/wanandsway.js
@@ -0,0 +1,47 @@
+import { World } from 'src/world.js';
+
+// * ALIASES
+const Application = PIXI.Application,
+	Container = PIXI.Container,
+	loader = PIXI.Loader.shared,
+	TextureCache = PIXI.utils.TextureCache,
+	Sprite = PIXI.Sprite;
+
+// * CREATE APPLICATION
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+const app = new Application({
+	antialias: true,
+	resizeTo: window
+});
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoDensity = true;
+document.body.appendChild(app.view);
+
+// * GLOBALS
+// ** CONSTANTS
+let state;
+
+// ** OBJECTS
+let map;
+
+let setup = () => {
+	// $('#console').text('set up');
+	// map = new World(app);
+	// $('#console').text('got here');
+	
+	state = single_player;
+	app.ticker.add(game_loop);
+}
+
+loader
+	.add('images/assets.json')
+	.load(setup);
+
+let game_loop = (delta) => {
+	state(delta);
+}
+
+let single_player = (delta) => {
+	
+}
