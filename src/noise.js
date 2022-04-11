d297e909798c91e45aae4f6220b9cb70264493be
lucac123 <lchackalackal@gmail.com>
2022-04-11 00:59:02 -0500

    did something

--- src/noise.js
+++ src/noise.js
@@ -1,51 +1,54 @@
-// const m = 65537,
-// 	  a = 75,
-// 	  c = 74;
-// const m = 4294967296, //2^32, period of pseudorandom runction
-// 	  a = 1664525, //seed value should be between 0 and modulus-1
-// 	  c = 1;
-const m = 2147483647, //made m prime for computation
-	  a = 48271,
-	  c = 0;
+import {M, A} from './constants.js';
+
+export {Noise};
 	  
-const grid_size = 5;
 
 class Noise {
 	constructor(seed) {
-		this.points = [];
-		$('#console').append('<br/>constructing noise');
 		this.seed = seed;
 		this.x = seed;
-		$('#console').append('<br/>set instance variables');
-		this.points[0] = this.rand();
-		this.points[1] = this.rand();
-		$('#console').append('<br/>did this');
-		$('#console').append('<br/>finished constructor');
+		// * USED BY PERLIN FUNCTION
+		// this.wavelength = wavelength;
+		// this.points = [this.rand(), this.rand()];
 	}
-
 	
-	perlin(x) {
-		let first = Math.floor(x/5);
-		let second = Math.ceil(x/5);
-		$('#console').append(`<br/>ayo ${this.rand()}`);
-	}
-
+	// * RETURNS RANDOM VALUE FROM INDEX k USING EXPLICIT FORMULA
+	// ** It works but is inefficient
 	rand_x(k) {
-		return this.mod_pow(a, k, m) * (this.seed % m) % m;
+		return this.mod_pow(A, k, M) * (this.seed % M) % M / M;
 	}
-
+	
+	// * RETURNS NEXT RANDOM VALUE, OR CALCULATES NEXT BASED ON <rand_seed>
 	rand(rand_seed) {
 		if (rand_seed === undefined) {
-			this.x = (a * this.x) % m;
-			return this.x;
+			this.x = (A * this.x) % M;
+			return this.x / M;
 		}
-		return ((a * rand_seed) % m) / m;
+		return ((A * rand_seed) % M) / M;
 	}
-	interpolate(y1, y2, x) {
-		let x2 = (1 - Math.cos(x*Math.PI))/2;
-		return y1*(1-x2) + y2*x2;
-	}
+	
+	// * WORLD GEN BETTER WITHOUT PERLIN, REMOVED
+	// perlin(x) {
+	// 	let first = Math.floor(x/this.wavelength);
+	// 	if (!(this.points.length > first+1)) {
+	// 		for (let i = this.points.length; i <= first+1; i++) {
+	// 			this.points[i] = this.rand();
+	// 		}
+	// 	}
+	// 	return this.interpolate(this.points[first], this.points[first+1], x/this.wavelength-first);
+	// }
 
+
+
+	// *** HELPER FUNCTIONS ***
+
+	// * USED BY PERLIN FUNCTION
+	// interpolate(y1, y2, x) {
+	// 	let x2 = (1 - Math.cos(x*Math.PI))/2;
+	// 	return y1*(1-x2) + y2*x2;
+	// }
+
+	// * USED BY rand_x, TIME INTENSIVE AT HIGHER POWERS
 	mod_pow(base, exp, mod) {
 		if (mod == 1)
 			return 0;
@@ -56,7 +59,3 @@ class Noise {
 		return c;
 	}
 }
-
-
-
-export {Noise};
--- src/world.js
+++ src/world.js
@@ -1,27 +1,31 @@
 import { Noise } from './noise.js';
+import { SCALE, BLOCK, M } from './constants.js';
+
+export { World };
 
 // * ALIASES
-const Application = PIXI.Application,
-	Container = PIXI.Container,
-	loader = PIXI.Loader.shared,
+const Container = PIXI.Container,
 	TextureCache = PIXI.utils.TextureCache,
 	Sprite = PIXI.Sprite;
-const SCALE = 3;
-const BLOCK = 16*SCALE;
 
-export class World {
+class World {
 	constructor(app) {
-		$('#console').append('ayoooo');
-		let noise = new Noise(30980909);
-		$('#console').append('<br/>ayo got here');
-		noise.perlin(1);
-		$('#console').append('<br/>noise');
-		let car = new Sprite(TextureCache['galooeh.png']);
+		this.seed = Math.floor(Math.random()*M);
+		this.noise = new Noise(this.seed);
+		this.app = app;
+		this.map = new Container();
+		this.width = app.renderer.width;
+		this.height = app.renderer.height;
+		for (let i = 0; i < 100; i++) {
+			console.log((this.noise.rand() > 0.6)? 'grass' : 'road');
+		}
+	}
+	
+	draw() {
+		let car = new Sprite(TextureCache['road_bot_lane.png']);
 		car.scale.set(SCALE);
-		// road.position.set(200, 200);
-		$('#console').append('<br/>ayo');
-		$('#console').append(`<br/>car: ${car.scale}`);
-		app.stage.addChild(car);
-		$('#console').append('<br/><br/>finished constructing world');
+		car.position.set(200, 200);
+		this.app.stage.addChild(car);
+
 	}
 }
